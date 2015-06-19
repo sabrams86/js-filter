3c154722d58a8ce2f9173ac96184a61a2cbeb577
Steven Abrams <steven.abrams86@gmail.com>
2015-06-18 18:51:17 -0600

    more mongolab config

--- connection.js
+++ connection.js
@@ -1,3 +1,6 @@
 var monk = require('monk');
-var db = monk(process.env.MONGOLAB_URI || 'localhost/album_class_demo');
+//UNCOMMENT FOR LOCALHOST
+// var db = monk('localhost:27017/cabins');
+//UNCOMMENT FOR HEROKU DEPLOY!!!
+var db = monk('mongolab.com:47602/heroku_dzlm604k');
 module.exports = db;
